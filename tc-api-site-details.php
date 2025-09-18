fccc4a6a3bb71ccf292242760ac683c382c0605d
Alex <remstroyod@gmail.com>
2025-09-18 14:14:44 +0300

    Add Amin Url

--- tc-api-site-details.php
+++ tc-api-site-details.php
@@ -4,7 +4,7 @@
  *
  * Plugin Name: TC Api Site Details
  * Description: Информация о сайте
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: TrafficConnect
  */
 
@@ -18,7 +18,7 @@ class ApiSiteDetails
 
     public function __construct()
     {
-
+        //
     }
 
     public function init()
@@ -71,11 +71,10 @@ class ApiSiteDetails
             'is_static_site_plugin_active' => $this->isStaticSitePluginActive(),
             'is_hb_waf_plugin_active' => $this->isHbWafPluginActive(),
             'is_pretty_links_plugin_active' => $this->getPrettyLinksPlugin(),
+            'admin_url' => $this->getAdminURL(),
         ]);
 
     }
-
-    //
 
     //Get Server IP
     private function getServerIP()
@@ -260,10 +259,46 @@ class ApiSiteDetails
         ];
     }
 
+    //Get Admin Url
+    private function getAdminURL()
+    {
 
+        $login_url = home_url('/wp-admin/');
 
+        if (!function_exists('is_plugin_active')) {
+            include_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
 
+        if (is_plugin_active('solid-security/solid-security.php')
+            || is_plugin_active('better-wp-security/better-wp-security.php')) {
 
+            // New Version Solid Security
+            $data = get_option('itsec-storage');
+            $slug = '';
+            $enabled = false;
+
+            if (is_array($data) && !empty($data['hide-backend'])) {
+                $slug    = $data['hide-backend']['slug'] ?? '';
+                $enabled = !empty($data['hide-backend']['enabled']);
+            }
+
+            // Old Version iThemes Security
+            if (!$slug || !$enabled) {
+                $data = get_option('itsec_hide_backend');
+                if (is_array($data)) {
+                    $slug    = $data['slug'] ?? '';
+                    $enabled = !empty($data['enabled']);
+                }
+            }
+
+            if ($enabled && !empty($slug)) {
+                $login_url = home_url('/' . ltrim($slug, '/') . '/');
+            }
+
+        }
+
+        return $login_url;
+    }
 
 }
 
